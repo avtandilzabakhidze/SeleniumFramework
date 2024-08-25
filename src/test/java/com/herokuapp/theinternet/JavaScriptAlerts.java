25c8c2876c23c8da5d94da578960b8238b476258
avtandilzabakhidze <zabakhidze01@gmail.com>
2024-08-25 18:51:43 +0400

    Change JavaScriptAlerts class

--- src/test/java/com/herokuapp/theinternet/JavaScriptAlerts.java
+++ src/test/java/com/herokuapp/theinternet/JavaScriptAlerts.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -25,10 +26,10 @@ public class JavaScriptAlerts {
         driver.get("https://the-internet.herokuapp.com/");
     }
 
-//    @AfterMethod
-//    public void tearDown() {
-//        driver.quit();
-//    }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 
     @Test
     public void JSAlert() {
